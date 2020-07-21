84d16ff22421b6aabee20dda357f297fab867763
Caroline Ignácio <carolineignacior@gmail.com>
2020-07-21 14:14:17 -0300

    Updates

---
+++ precache-manifest.32e0f30a929167e32eaf6b994a18f5dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a65d4a050218ed7f5802fe88aa8371",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "ed541a9f7262d65547d8",
+    "url": "/quiz-app/static/js/2.bc3f9067.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-app/static/js/2.bc3f9067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fc8f882ad300bc8989a",
+    "url": "/quiz-app/static/js/main.535b39b8.chunk.js"
+  },
+  {
+    "revision": "99d76282e7b113247ce6",
+    "url": "/quiz-app/static/js/runtime-main.1e859166.js"
+  }
+]);
